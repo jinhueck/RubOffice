04789886e6fae00712443f3eff3f32f470778623
hychan0517 <gjdbscks89@naver.com>
2020-06-08 15:35:13 +0900

    [HYC]Scroll코드추가

---
+++ RubOffice/Assets/Scripts/HYC_Scripts/ScrollSlotBase.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public abstract class ScrollSlotBase : MonoBehaviour
+{
+	public abstract RectTransform GetRectTransform();
+	public abstract void SettingSlot(object data);
+}
