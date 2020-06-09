1da4ad6503dab90f76d4641ce54c93fa75974d32
hychan0517 <gjdbscks89@naver.com>
2020-06-09 11:28:48 +0900

    [HYC] 사용할 코드 정리

--- RubOffice/Assets/Scripts/HYC_Scripts/Global/ScrollSlotBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class ScrollSlotBase : MonoBehaviour
-{
-	public abstract RectTransform GetRectTransform();
-	public abstract void SettingSlot(object data);
-}
